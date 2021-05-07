644bc0499c1f70d02bf8c66a5a4ec86fb40d52e4
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-07 17:15:57 -0400

    Update server.sh

--- server.sh
+++ server.sh
@@ -11,15 +11,23 @@ sudo chmod +x pktriot
 echo $PKTRIOT>pktriot.json
 ./pktriot --config ./pktriot.json start &
 cd server
+7z x freeop.7z
 } &> /dev/null
-echo running server! also sleeping for 5hrs...
+echo running servers! also sleeping for 5hrs...
 {
-java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui &
+java -Xmx512M -Xms512M -jar BungeeCord.jar nogui &
+cd lobby
+java -Xmx2048M -Xms2048M -jar server.jar nogui &
+cd ..
+cd freeop
+java -Xmx2048M -Xms2048M -jar server.jar nogui &
 cd ..
 sleep 5h
 } &> /dev/null
 echo uploading to dropbox...
 {
+rm -rf ./freeop
+cd ..
 7z a server.7z ./server
 curl -X POST https://content.dropboxapi.com/2/files/upload --header "Authorization: Bearer $DROPBOX" --header "Dropbox-API-Arg: {\"path\": \"/server.7z\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}" --header "Content-Type: application/octet-stream" --data-binary @server.7z
 } &> /dev/null
