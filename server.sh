870a6faf2bf19e904fc79d07d0ff3bee3545506d
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-07 16:46:34 -0400

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-sleep 5
+echo downloading from dropbox...
+{
 curl -X POST https://content.dropboxapi.com/2/files/download --header "Authorization: Bearer $DROPBOX" --header "Dropbox-API-Arg: {\"path\": \"/server.7z\"}" -o server.7z
 7z x server.7z
 rm -rf server.7z
-#sudo chmod +x pktriot
-#echo $PKTRIOT>pktriot.json
-#./pktriot --config ./pktriot.json start > /dev/null &
-#echo Starting!
+} &> /dev/null
+echo running pktriot...
+{
+sudo chmod +x pktriot
+echo $PKTRIOT>pktriot.json
+./pktriot --config ./pktriot.json start &
 cd server
-echo $RANDOM>test.txt
-#java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui > /dev/null
+} &> /dev/null
+echo running server! also sleeping for 5hrs...
+{
+java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui &
 cd ..
-sleep 15
+sleep 5h
+} &> /dev/null
+echo uploading to dropbox...
+{
 7z a server.7z ./server
 curl -X POST https://content.dropboxapi.com/2/files/upload --header "Authorization: Bearer $DROPBOX" --header "Dropbox-API-Arg: {\"path\": \"/server.7z\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}" --header "Content-Type: application/octet-stream" --data-binary @server.7z
+} &> /dev/null
+echo done! looping...
