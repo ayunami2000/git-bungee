f4c8208f067c917747378607b50e317d677c31b5
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-07 15:52:09 -0400

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #apt update
 #apt install p7zip-full
+sleep 5
 curl -X POST https://content.dropboxapi.com/2/files/download --header "Authorization: Bearer $DROPBOX" --header "Dropbox-API-Arg: {\"path\": \"/server.7z\"}"
 ls
 #sudo chmod +x pktriot
