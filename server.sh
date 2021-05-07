5b40a1c793e767953f02b4e016564003f79c56c1
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-07 15:50:06 -0400

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-wget -O minecraft_server.jar https://papermc.io/api/v1/paper/1.16.5/latest/download
-unzip server.zip
-wget -O plugins/IllegalStack.jar https://api.spiget.org/v2/resources/44411/download
-echo eula=true>eula.txt
-sudo chmod +x pktriot
-echo $PKTRIOT>pktriot.json
-./pktriot --config ./pktriot.json start > /dev/null &
-echo Starting!
-java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui > /dev/null
+#apt update
+#apt install p7zip-full
+curl -X POST https://content.dropboxapi.com/2/files/download --header "Authorization: Bearer $DROPBOX" --header "Dropbox-API-Arg: {\"path\": \"/server.7z\"}"
+ls
+#sudo chmod +x pktriot
+#echo $PKTRIOT>pktriot.json
+#./pktriot --config ./pktriot.json start > /dev/null &
+#echo Starting!
+#java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui > /dev/null
