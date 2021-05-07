94f4d286277dcfb68e6d31c97f0be77dda55c332
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-07 15:57:46 -0400

    Update server.sh

--- server.sh
+++ server.sh
@@ -9,3 +9,6 @@ ls
 #./pktriot --config ./pktriot.json start > /dev/null &
 #echo Starting!
 #java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui > /dev/null
+sleep 15
+echo $RANDOM>bruh.txt
+curl -X POST https://content.dropboxapi.com/2/files/upload --header "Authorization: Bearer $DROPBOX" --header "Dropbox-API-Arg: {\"path\": \"/bruh.txt\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}" --header "Content-Type: application/octet-stream" --data-binary @bruh.txt
